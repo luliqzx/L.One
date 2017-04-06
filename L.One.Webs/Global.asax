25d9617688e200ba50fa074ece36d72caf747c6a
luliqzx <luliqzx@gmail.com>
2017-04-06 13:46:16 +0700

    webapp

---
+++ L.One.Webs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L.One.Webs.MvcApplication" Language="C#" %>
---
+++ L.One.Webs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L.One.Webs.Startup))]
+namespace L.One.Webs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
